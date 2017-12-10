dfc49af81c538c56272b4f8c9a7dc48276edea8f
jazz mickle <jaymickle@gmail.com>
2017-12-10 12:47:12 +0100

    GUI stuff!
    Lots of work, restructure wallpaint as a class. Built all the GUI necessary for the tool. Preview option.

--- wallpaintgui.py
+++ wallpaintgui.py
@@ -2,6 +2,7 @@ from tkinter import *
 from tkinter.filedialog import askopenfilename
 import omg
 import drawmap
+import wallpaint
 from PIL import Image
 from PIL import ImageTk
 import threading
@@ -86,7 +87,196 @@ class LoadDialogue(Tk):
 
     def open_map(self):
         index = self.map_select.curselection()[0]
-        print(self.map_select.get(index))
+        MapView(None, self.path, self.map_select.get(index))
+
+
+class MapView(Tk):
+    def __init__(self, parent, wadpath, mapname):
+        Tk.__init__(self, parent)
+        self.title(mapname)
+        self.frame = Frame(self)
+        self.frame.grid(sticky="NEWS")
+        self.rowconfigure(0, weight=1)
+        self.columnconfigure(0, weight=1)
+        self.frame.rowconfigure(2, weight=1)
+        self.frame.columnconfigure(0, weight=1)
+        self.map_canvas = None
+        self.preview_button = None
+        self.clear_button = None
+        self.line_list_box = None
+
+        self.bg_color = "#221c23"
+        self.line_color = "#ac3232"
+        self.selected_color = "#4d6c30"
+        self.highlight_color = "#fcf337"
+        self.selected_highlight_color = "#9be753"
+
+        self.wp = wallpaint.Wallpaint(wadpath, mapname)
+
+        self.line_list = []
+
+        self.wadpath = wadpath
+        self.wad = omg.WAD(wadpath)
+        self.mapname = mapname
+
+        self.scale = 0.1
+
+        self.max_scale = 1.0
+        self.min_scale = 0.01
+        self.mouse_x = 0
+        self.mouse_y = 0
+
+        self.init_gui()
+        self.build_map()
+
+        # Event binding
+        self.map_canvas.bind(sequence="<ButtonPress-1>", func=self.on_mouse_down)
+        self.map_canvas.bind(sequence="<Motion>", func=self.on_mouse_move)
+        self.map_canvas.bind(sequence="<ButtonRelease-1>", func=self.on_mouse_up)
+        self.map_canvas.bind_all(sequence="<KeyPress>", func=self.on_key_down)
+        self.map_canvas.bind_all(sequence="<KeyRelease>", func=self.on_key_up)
+        # self.map_canvas.bind(sequence="<MouseWheel>", func=self.on_mouse_wheel)
+
+        self.dragging = False
+        self.mouse_down = False
+        self.last_pos = (0, 0)
+
+    def init_gui(self):
+        self.map_canvas = Canvas(self.frame, width=640, height=480, bg=self.bg_color)
+        self.map_canvas.grid(row=0, column=0, rowspan=3, sticky="NEWS")
+
+        self.preview_button = Button(self.frame, text="preview", command=self.create_preview)
+        self.preview_button.grid(row=0, column=1, sticky="NEW")
+
+        self.preview_button = Button(self.frame, text="clear", command=self.clear_lines)
+        self.preview_button.grid(row=1, column=1, sticky="NEW")
+
+        self.line_list_box = Listbox(self.frame, width=6)
+        self.line_list_box.grid(row=2, column=1, sticky="NEWS")
+
+    def create_preview(self):
+        # print("wallpaint {} {}".format(self.mapname, self.line_list))
+        lines = []
+        for l in self.line_list:
+            lines.append(int(l)-1)
+        self.wp.preview(lines)
+
+    def clear_lines(self):
+        self.line_list = []
+        self.line_list_box.delete(0, END)
+        self.update_line_colors()
+
+    def build_map(self):
+        minx = 10000
+        miny = 10000
+        maxx = -10000
+        maxy = -10000
+        mapeditor = omg.MapEditor(self.wad.maps[self.mapname])
+
+        for l in mapeditor.linedefs:
+            vx_a = mapeditor.vertexes[l.vx_a]
+            vx_b = mapeditor.vertexes[l.vx_b]
+
+            minx = min(minx, vx_a.x)
+            miny = min(miny, vx_a.y)
+            maxx = max(maxx, vx_a.x)
+            maxy = max(maxy, vx_a.y)
+            minx = min(minx, vx_b.x)
+            miny = min(miny, vx_b.y)
+            maxx = max(maxx, vx_b.x)
+            maxy = max(maxy, vx_b.y)
+
+            self.map_canvas.create_line(vx_a.x * self.scale,
+                                        -vx_a.y * self.scale,
+                                        vx_b.x * self.scale,
+                                        -vx_b.y * self.scale,
+                                        fill=self.line_color,
+                                        tags=mapeditor.linedefs.index(l) + 1)
+
+        self.map_canvas.move("all", self.map_canvas.winfo_reqwidth() / 2, self.map_canvas.winfo_reqheight() / 2)
+
+    def close(self):
+        self.destroy()
+
+    # Event handlers
+    def on_key_down(self, event):
+        if event.char == " ":
+            self.dragging = True
+
+        if event.char == "]":
+            self.scale_map(1)
+
+        if event.char == "[":
+            self.scale_map(-1)
+
+        if event.char == "c":
+            self.clear_lines()
+
+    def scale_map(self, delta):
+        last_scale = self.scale
+        if self.max_scale > (self.scale + (delta * 0.1)) > self.min_scale:
+            self.scale += (delta * 0.1)
+            delta = self.scale / last_scale
+            self.map_canvas.scale("all", self.mouse_x, self.mouse_y, delta, delta)
+
+    def on_key_up(self, event):
+        if event.char == " ":
+            self.dragging = False
+
+    def on_mouse_down(self, event):
+
+        closest = self.map_canvas.find_closest(self.mouse_x, self.mouse_y)
+        line_id = self.map_canvas.gettags(closest)[0]
+
+        if line_id not in self.line_list:
+            self.line_list.append(line_id)
+            self.line_list_box.insert(END, line_id)
+        else:
+            index = self.line_list.index(line_id)
+            del self.line_list[index]
+            self.line_list_box.delete(index)
+
+        self.update_line_colors()
+
+        self.mouse_down = True
+
+    def on_mouse_up(self, event):
+        # self.update_cursor(event)
+        self.mouse_down = False
+
+    def on_mouse_wheel(self, event):
+        print("is this happening")
+        last_scale = self.scale
+        if self.max_scale > (self.scale + (event.delta * 0.1)) > self.min_scale:
+            self.scale += (event.delta * 0.1)
+            delta = self.scale / last_scale
+            self.map_canvas.scale("all", event.x, event.y, delta, delta)
+
+    def on_mouse_move(self, event):
+        move = (event.x - self.last_pos[0], event.y - self.last_pos[1])
+        self.last_pos = (event.x, event.y)
+        self.mouse_x = event.x
+        self.mouse_y = event.y
+
+        # Canvas dragging
+        if self.dragging:
+            self.map_canvas.move("all", move[0], move[1])
+
+        self.update_line_colors()
+
+    def update_line_colors(self):
+        # Highlight nearest line
+        self.map_canvas.itemconfig("all", fill=self.line_color, width=1.0)
+
+        for l in self.line_list:
+            self.map_canvas.itemconfig(self.map_canvas.find_withtag(l)[0], fill=self.selected_color, width=1.0)
+
+        closest = self.map_canvas.find_closest(self.mouse_x, self.mouse_y)
+        if self.map_canvas.gettags(closest)[0] in self.line_list:
+            self.map_canvas.itemconfig(closest, fill=self.selected_highlight_color, width=2.0)
+        else:
+            self.map_canvas.itemconfig(closest, fill=self.highlight_color, width=2.0)
+
 
 
 if __name__ == "__main__":
